7cc28d583695c28354b694bc6e94c1e0d268174e
Michel Zwahr <michelzwahr@gmail.com>
2026-05-10 19:47:29 +0200

    feat: add initial html and javascript files

---
+++ static/js/test_formular.js
@@ -0,0 +1,23 @@
+document.getElementById("testform").addEventListener("submit", function(event) {
+    event.preventDefault(); // verhindert Seitenreload
+
+    const fname = document.getElementById("fname").value;
+    const name = document.getElementById("name").value;
+
+    fetch("/submit", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            fname: fname,
+            name: name
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        window.location.href = "/success.html";
+    });
+
+});
